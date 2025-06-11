228e3ae26e3d8959da5644ac22fe4fdff90e02ee
JonasMahlburg <darrylmanga@yahoo.com>
2025-06-11 09:51:46 +0200

    feat: add two example tests for a 200_OK and 400_BAD_REQUEST

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class LikeTests(APITestCase):
+   
+    def test_get_like(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+
+    def test_get_likes(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+        
+
