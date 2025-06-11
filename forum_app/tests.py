918fa024ae1257b3c84473be86df04b04dd388c0
JonasMahlburg <darrylmanga@yahoo.com>
2025-06-11 11:59:01 +0200

    feat: add QuestionTest with detail_question and Post-test

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,20 +1,51 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
+from .models import Question
+from .api.serializers import QuestionSerializer
 
 
 class LikeTests(APITestCase):
    
     def test_get_like(self):
-        url = reverse('like-list')
+        url = 'http://127.0.0.1:8000/api/forum/likes/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
 
-    def test_get_likes(self):
-        url = reverse('like-list')
+    # def test_get_likes(self):
+    #     url = reverse('like-list')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+class QuestionTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.question = Question.objects.create(title='Test Question', content='Test Content', author=self.user, category='frontend')
+        self.client = APIClient()
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_list_post_question(self):
+        url = reverse('question-list')
+        data = {'title': 'Question1',
+                'content': '1Content',
+                'author': self.user.id,
+                'category': 'frontend'
+                }
+        response = self.client.post(url, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    
+    def test_detail_question(self):
+        url = reverse('question-detail', kwargs={'pk': self.question.id})
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-        
+        expected_data = QuestionSerializer(self.question).data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertDictEqual(response.data, expected_data)
+        self.assertJSONEqual(response.content, expected_data)
+        self.assertContains(response, 'title')
 
